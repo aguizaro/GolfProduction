410b4431628446e9e1bda7706fc91557cc26c23e
OVO <lzx213@sina.com>
2024-04-25 14:31:05 -0700

    add Chinese Localization

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -103,6 +103,7 @@ public class UIManager : MonoBehaviour
         instance = this;
 
         RefreshDisplayList();
+        InitializetLanguageDropdown();
     }
 
     private void Start() { DisablePause(); DisableSettings(); EnableUI(UIState.Title); }
@@ -174,6 +175,21 @@ public class UIManager : MonoBehaviour
     public void SetOneHandModeToggle(bool value) => oneHandMode = value;
     public void SetLanguageDropdown(int value) => language = value;
 
+    private void InitializetLanguageDropdown()
+    {
+        var currentLocale = LocalizationSettings.SelectedLocale;
+        var locales = LocalizationSettings.AvailableLocales.Locales;
+        for (int i = 0; i < locales.Count; i++)
+        {
+            if (locales[i] == currentLocale)
+            {
+                _settingsLanguageDropdown.value = i;
+            }
+        }
+
+
+    }
+
     public void LoadSettings()
     {
         // Load settings data
@@ -185,7 +201,7 @@ public class UIManager : MonoBehaviour
         language = sData.language;
 
         _settingsSensitivitySlider.value = settingsSensitivity;
-        _settingsLanguageDropdown.value = language;
+        //_settingsLanguageDropdown.value = language;
     }
 
     public void ApplySettings()
