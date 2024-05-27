c116345934de6725b17bb925c3021243975f91df
John Perone <132640078+jperoneucsc@users.noreply.github.com>
2024-05-27 15:18:07 -0700

    Implemented the stand up animations #35
    * getup animation
    
    * basic get up anim + animation tweaks
    
    * scenechanges
    
    * in progress work
    
    * In progress
    
    * working, needs standup2 animation and fix AlignRotation fx
    
    * new standupanim
    
    * anim work
    
    * Finished get up anim system
    
    * remove cheat keys
    
    * getup animation
    
    * basic get up anim + animation tweaks
    
    * scenechanges
    
    * in progress work
    
    * In progress
    
    * working, needs standup2 animation and fix AlignRotation fx
    
    * new standupanim
    
    * anim work
    
    * Finished get up anim system
    
    * Merge branch 'reverseRagdoll' of https://github.com/aguizaro/GolfProduction into reverseRagdoll

--- Assets/Scripts/SwingManager.cs
+++ Assets/Scripts/SwingManager.cs
@@ -143,7 +143,7 @@ public class SwingManager : NetworkBehaviour
                 playerAnimator.ResetTrigger("Stance");
             }
 
-            return; // Don't execute further logic
+            return;
         }
 
     }
@@ -236,7 +236,6 @@ public class SwingManager : NetworkBehaviour
             targetPosition = thisBall.transform.position + (-playerTransform.forward * 0.12f) + playerTransform.right * -.75f;
         }
 
-        //targetPosition.y -= 0.12f;    // Instead of moving targ pos down, use a raycast to touch the ground
         // Perform a raycast downwards to find the ground position beneath the target position
         RaycastHit hit;
         if (Physics.Raycast(targetPosition, Vector3.down, out hit, 3))
